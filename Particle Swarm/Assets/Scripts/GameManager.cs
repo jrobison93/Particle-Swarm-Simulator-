f57553fc696f0922c4d17bbdec5b01a14e227f0b
John Robison <10904512@live.mercer.edu>
2016-04-23 23:26:49 -0400

    Created the particle prefab and script.

---
+++ Particle Swarm/Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject particle;
+
+    private SwarmParticle[] particleSwarm;
+    private Vector2 globalBest;
+    private Vector2 targetPosition;
+
+	// Use this for initialization
+	void Start ()
+    {
+        InitializeSwarm();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        MoveSwarm();
+	
+	}
+
+    void MoveSwarm()
+    {
+
+    }
+
+    void InitializeSwarm()
+    {
+
+    }
+
+    public Vector2 GetTarget()
+    {
+        return targetPosition;
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Clicked");
+        Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log("Position: " + position.x + ", " + position.y + ", " + position.z);
+    }
+}
---
+++ Particle Swarm/Assets/Scripts/SwarmParticles.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwarmParticle : MonoBehaviour
+{
+    double bestFitness;
+    Vector3 bestPosition;
+    Vector3 velocity;
+
+
+	public SwarmParticle(float posx, float posy, float velx, float vely)
+    {
+        transform.position.Set(posx, posy, transform.position.z);
+        velocity = new Vector3(velx, vely, 0);
+        bestFitness = double.MaxValue;
+    }
+
+    public void Move(Vector2 target, Vector2 globalBest, bool newTarget)
+    {
+        transform.position = transform.position + (velocity * Time.deltaTime);
+
+        if(newTarget || GetFitness(target) < bestFitness)
+        {
+            bestFitness = GetFitness(target);
+            bestPosition = transform.position;
+        }
+
+        velocity.x += (0.5f * Random.Range(0, 1) * (globalBest.x - transform.position.x)) + (0.5f * Random.Range(0, 1) * (bestPosition.x - transform.position.x));
+        velocity.y += (0.5f * Random.Range(0, 1) * (globalBest.y - transform.position.y)) + (0.5f * Random.Range(0, 1) * (bestPosition.y - transform.position.y));
+
+    }
+
+    public double GetFitness(Vector2 target)
+    {
+        return Vector2.Distance(transform.position, target);
+    }
+}
