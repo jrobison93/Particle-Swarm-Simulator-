2ad5250f762c46dfbda5a7c50a1557b7e16ba4b3
John Robison <10904512@live.mercer.edu>
2016-04-24 00:37:24 -0400

    Finished the simulation code.

--- Particle Swarm/Assets/Scripts/GameManager.cs
+++ Particle Swarm/Assets/Scripts/GameManager.cs
@@ -4,35 +4,63 @@ using System.Collections;
 public class GameManager : MonoBehaviour
 {
     public GameObject particle;
-
-    private SwarmParticle[] particleSwarm;
-    private Vector2 globalBest;
-    private Vector2 targetPosition;
+    public int numParticles = 5;
+    
+    private ArrayList particleSwarm;
+    private Vector3 globalBestPosition;
+    private double globalBestFitness;
+    private Vector3 targetPosition;
+    private bool newTarget;
+    private float vert;
+    private float hori;
 
 	// Use this for initialization
 	void Start ()
     {
+        vert = Camera.main.orthographicSize;
+        hori = vert * Screen.width / Screen.height;
+        targetPosition = new Vector2(0f, 0f);
+        globalBestFitness = double.MaxValue;
+        newTarget = true;
         InitializeSwarm();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        MoveSwarm();
-	
+        MoveSwarm();	
 	}
 
     void MoveSwarm()
     {
+        foreach(GameObject part in particleSwarm)
+        {
+            SwarmParticle particle = part.GetComponent<SwarmParticle>();
+            particle.Move(targetPosition, globalBestPosition, newTarget);
 
+            if(particle.GetFitness(targetPosition) < globalBestFitness)
+            {
+                globalBestFitness = particle.GetFitness(targetPosition);
+                globalBestPosition = particle.transform.position;
+            }
+        }
+        newTarget = false;
     }
 
     void InitializeSwarm()
     {
+        particleSwarm = new ArrayList();
+        for(int i = 0; i < numParticles; i++)
+        {
+            GameObject tempParticle = Instantiate(particle) as GameObject;
+            tempParticle.GetComponent<SwarmParticle>().SetUp(Random.Range(-hori, hori),
+                Random.Range(-vert, vert), Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f));
+            particleSwarm.Add(tempParticle);
+        }
 
     }
 
-    public Vector2 GetTarget()
+    public Vector3 GetTarget()
     {
         return targetPosition;
     }
@@ -41,6 +69,9 @@ public class GameManager : MonoBehaviour
     {
         Debug.Log("Clicked");
         Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        targetPosition = position;
+        globalBestFitness = double.MaxValue;
         Debug.Log("Position: " + position.x + ", " + position.y + ", " + position.z);
+        newTarget = true;
     }
 }
---
+++ Particle Swarm/Assets/Scripts/SwarmParticle.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwarmParticle : MonoBehaviour
+{
+    double bestFitness;
+    Vector3 bestPosition;
+    Vector3 velocity;
+
+
+	public void SetUp(float posx, float posy, float velx, float vely)
+    {
+        transform.position = new Vector3(posx, posy, 0);
+        velocity = new Vector3(velx, vely, 0);
+        bestFitness = double.MaxValue;
+    }
+
+    public void Move(Vector3 target, Vector2 globalBest, bool newTarget)
+    {
+        transform.position = transform.position + (velocity * Time.deltaTime);
+
+        //Debug.Log(target);
+
+        if(newTarget || (GetFitness(target) < bestFitness))
+        {
+            bestFitness = GetFitness(target);
+            bestPosition = transform.position;
+        }
+
+        velocity.x += (0.25f * Random.Range(0f, 1f) * (globalBest.x - transform.position.x)) + (0.25f * Random.Range(0f, 1f) * (bestPosition.x - transform.position.x));
+        velocity.y += (0.25f * Random.Range(0f, 1f) * (globalBest.y - transform.position.y)) + (0.25f * Random.Range(0f, 1f) * (bestPosition.y - transform.position.y));
+
+    }
+
+    public double GetFitness(Vector3 target)
+    {
+        return Vector3.Distance(transform.position, target);
+    }
+}
